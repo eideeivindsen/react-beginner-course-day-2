8daa23f8d81052d06128a528057c0e44838b1bba
Jørgen <jorgensta@gmail.com>
2020-10-19 15:44:28 +0200

    Update 02-http/src/App.jsx

--- 02-http/src/App.jsx
+++ 02-http/src/App.jsx
@@ -10,26 +10,8 @@ const byUrls = {
 
 const App = () => {
 
-  const [bysykkelData, setBysykkelData] = useState(null);
-  const [leveringstidAvData, setLeveringstidAvData] = useState(null);
-
-  useEffect(() => {
-    console.log("Henter bysykkeldata fra Oslo");
-    const t0 = performance.now()
-
-    fetch(byUrls.oslo)
-      .then(res => res.json())
-      .then(data => setBysykkelData(data))
-      .finally(() => setLeveringstidAvData(performance.now() - t0));
-
-  }, [])
-
-  console.log(bysykkelData);
-
   return <div className="App">
     <h1>Bysykkeldata</h1>
-    {bysykkelData && <h1>Antall stasjoner i Oslo: {bysykkelData.data.stations.length}</h1>}
-    {leveringstidAvData && <h2>Leveringstid: {Math.floor(leveringstidAvData)}ms</h2>}
   </div>
 }
 

dc782db48cc5368dacafd6ea4ee46ab180939ce8
Amund Tenstad <github@amund.io>
2020-10-20 09:06:48 +0200

    Update 00-recap

--- 00-recap/src/App.jsx
+++ 00-recap/src/App.jsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import Avatar from "./Avatar";
 import pokemonList from "./pokemon";
 
-const Pokemon = ({ first, second, third }) => {
-  const [pokeState, setPokeState] = useState(0);
-
-  const handleClick = () => {
-    setPokeState(pokeState < 2 ? pokeState + 1 : 0);
-  }
-
-  return <div onClick={handleClick}>
-    {pokeState === 0 && <Avatar {...first} />}
-    {pokeState === 1 && <Avatar {...second} />}
-    {pokeState === 2 && <Avatar {...third} />}
-  </div>
-}
 
 const App = () => <>
-  {pokemonList.map(({ id, ...pokemon }) => <Pokemon key={id} {...pokemon} />)}
+  
 </>
 
 export default App;
